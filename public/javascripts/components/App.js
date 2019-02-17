083f5877687c687571d4ce9520476d19da58c9ce
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2019-02-16 23:07:23 -0500

    setup App.js, store asset files (css & woff2) locally

--- public/javascripts/components/App.js
+++ public/javascripts/components/App.js
@@ -1,10 +1,37 @@
+const SearchContainer = () => (
+  <main id='search-container'>Search</main>
+)
+
+const FavoritesContainer = () => (
+  <main id='favorites-container'>Favorites</main>
+)
+
 class App extends React.Component {
+  state = {
+    showFavorites: false
+  }
+
   render() {
     return (
       <div id='app'>
-        <header>
-          OMDb Tracker
-        </header>
+        <header>OMDb Movie Tracker</header>
+        <nav>
+          <a 
+            href='#'
+            onClick={() => this.setState({ showFavorites: false })}
+          >Search
+          </a>
+          <a 
+            href='#'
+            onClick={() => this.setState({ showFavorites: true })}
+          >Favorites
+          </a>
+        </nav>
+        {
+          (this.state.showFavorites)
+            ? <FavoritesContainer />
+            : <SearchContainer />
+        }
       </div>
     )
   }

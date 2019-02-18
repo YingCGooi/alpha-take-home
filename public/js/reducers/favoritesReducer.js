9fbf05dfdc5a9e299d22364d367d0cad040b125d
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2019-02-17 23:49:42 -0500

    change star wars rating to 3

--- public/js/reducers/favoritesReducer.js
+++ public/js/reducers/favoritesReducer.js
@@ -4,7 +4,7 @@ const initialState = {
     "year": "1977",
     "plot": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
     "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    "rating": 5,
+    "rating": 3,
     "comments": "Awesome movie!"
   },
   "tt0499549": {
@@ -19,7 +19,7 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-        
+
     default: return state;
   }
   return state;

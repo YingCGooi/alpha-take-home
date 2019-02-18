1515489a11b2ff425c2fd9a684e665e430088ec9
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2019-02-17 22:40:48 -0500

    render movie items for each favorited movie

--- public/js/components/FavoritesContainer.js
+++ public/js/components/FavoritesContainer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import MovieItem from './MovieItem';
 
-const FavoritesContainer = () => (
+const FavoritesContainer = ({ favorites }) => (
   <main id='favorites-container'>
-    <h2>Favorites List</h2>
+    <ul>
+    {
+      Object.keys(favorites).map(imdbId => (
+        <li key={imdbId}>
+          <MovieItem movie={ favorites[imdbId] } isFavorite={true} />
+        </li>
+      ))
+    }
+    </ul>
   </main>
 );
 
-export default FavoritesContainer;
+const mapStateToProps = (state) => (
+  { favorites: state.favorites }
+)
+
+export default connect(mapStateToProps)(FavoritesContainer);

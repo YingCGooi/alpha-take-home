c6ad7921ef7a794165a5204626f57cf94d43bef0
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2019-02-17 22:43:29 -0500

    rename Movie to MovieItem

---
+++ public/js/components/MovieItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import FavoriteInfo from './FavoriteInfo';
+
+const MovieItem = ({ movie, isFavorite }) => (
+  <article className='movie'>
+    <div className='poster'>
+      {
+        (movie.poster === 'N/A')
+          ? <img src='http://via.placeholder.com/300x466' />
+          : <img src={ movie.poster } />
+      }
+    </div>
+    <div className='info'>
+      <div>
+        <h2>{ movie.title } <span>({ movie.year })</span></h2>
+        <p>{ movie.plot }</p>
+      </div>
+      {
+        (isFavorite) 
+          ? <FavoriteInfo rating={ movie.rating } comments={ movie.comments } />
+          : <button className='favorite'>Add To Favorites</button> 
+      }
+    </div>
+  </article>
+)
+
+export default MovieItem;
